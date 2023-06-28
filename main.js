2e227469acc2ed231d2149a5ad682c83bc9f0767
Lilianna Szabo <lilianna.szabo@softwire.com>
2023-06-28 10:57:31 +0100

    Added user input for List All and List[name]

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-const fs = require('fs');
-const readline = require('readline');
-const filePath = 'Transactions2014.csv'; // Replace with the actual file path
-const readStream = fs.createReadStream(filePath);
-const rl = readline.createInterface({
-    input: readStream,
-    crlfDelay: Infinity // To handle both Unix and Windows line endings
-});
+const csv = require('csv-parser')
+const fs = require('fs')
+
+const prompt = require('prompt-sync')();
+
 class Account {
     amount;
-    name;
+    holder;
     constructor(name) {
-        this.name = name;
+        this.holder = name;
+        this.amount = 0;
     }
     addMoney(amountToBeAdded){
         this.amount += amountToBeAdded
@@ -24,41 +22,165 @@ class AccountManager{
     }
 
     findAccountByName(name){
+        let accountResult = null;
         this.accounts.forEach(account =>{
-                if(account.name === name){
-                    return account;
+                if(account.holder === name){
+                    accountResult = account;
                 }
             }
         )
-        return null;
+        return accountResult;
     }
 
     existsAccount(name){
+        let exists = false
         this.accounts.forEach(account =>{
-                if(account.name === name){
-                    return true;
+                if(account.holder === name){
+                    exists = true;
                 }
             }
         )
-        return false;
+        return exists;
     }
 
 }
-let accountManager = new AccountManager();
-rl.on('line', (line) => {
-    var data = line.split(',')
-    console.log('Line:', line);
-    if(!accountManager.existsAccount(line[1])){
-        accountManager.addAccount(new Account(line[1]));
+
+class TransactionManager{
+    transactions = [];
+
+    addTransaction(transaction){
+        this.transactions.push(transaction);
     }
 
-});
+    getTransactionByName(name){
+        let result = [];
+        this.transactions.forEach(transaction =>{
+                if (transaction.to === name || transaction.from === name){
+                    result.push(transaction)
+                }
+            }
 
-rl.on('close', () => {
-    console.log('Finished reading the file');
-});
+        )
+        return result;
+    }
+}
+
+class Transaction{
+    amount;
+    date;
+    from;
+    to;
+    narrative;
+
+    constructor(date, from, to, narrative, amount) {
+        this.date = date;
+        this.from = from;
+        this.to = to;
+        this.narrative = narrative;
+        this.amount = amount;
+    }
+
+}
+
+let results = [];
+function processAccounts(results) {
+    let accountManager = new AccountManager();
+    results.forEach(transaction =>{
+        let fromName = transaction["From"];
+        let toName = transaction["To"];
+        let fromAccount;
+        let toAccount;
+        //Create accounts
+        if(!accountManager.existsAccount(fromName)){
+            fromAccount = new Account(fromName);
+            accountManager.addAccount(fromAccount)
+        } else{
+            fromAccount = accountManager.findAccountByName(fromName);
+        }
+        if(!accountManager.existsAccount(toName)){
+            toAccount = new Account(toName);
+            accountManager.addAccount(toAccount)
+        } else{
+            toAccount = accountManager.findAccountByName(toName);
+
+        }
+        let money = parseFloat(transaction["Amount"])
+        fromAccount.addMoney(money * -1)
 
 
+        toAccount.addMoney(money)
+
+    })
+
+    return accountManager;
+
+}
+function processTransactions(results) {
+    let transactionManager = new TransactionManager();
+    results.forEach(line =>{
+        let transaction = new Transaction(line["Date"], line["From"], line["To"], line["Narrative"], parseFloat(line["Amount"]))
+        transactionManager.addTransaction(transaction)
+    })
+    return transactionManager;
+
+}
+
+
+function getUserInput() {
+    let userInput = "";
+    userInput = prompt("Please enter \'List All\' or \'List[Account_name]\' ");
+    return userInput;
+}
+
+function logAllAccounts(accountManager) {
+    accountManager.accounts.forEach(account => {
+            console.log("Account name: " + account.holder + "\n"
+                + "Account balance: " + account.amount + "\n"
+                + "---------------------------------------------")
+        }
+    )
+}
+
+function logAllTransactions(name, transactionManager) {
+    let results = [];
+    transactionManager.transactions.forEach(transaction => {
+        if(transaction.to === name || transaction.from === name){
+            results.push(transaction);
+        }
+    })
+    if(results.length === 0){
+        console.log("No transactions for that name");
+    }
+    else {
+        results.forEach(transaction => {
+            console.log(
+                "Transaction date: " + transaction.date + "\n" +
+                "From: " + transaction.from + "\n" +
+                "To: " + transaction.to + "\n" +
+                "Narrative: " + transaction.narrative + "\n" +
+                "Amount " + transaction.amount + "\n"
+            )
+        })
+    }
+}
+
+fs.createReadStream('Transactions2014.csv')
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+        let accountManager = processAccounts(results)
+        let transactionManager = processTransactions(results)
+
+        let userInput = getUserInput();
+        if(userInput === "List All"){
+            logAllAccounts(accountManager);
+        }
+        else if(userInput.substring(0,4) === "List"){
+            let userInputName = userInput.split("[")[1];
+            logAllTransactions(userInputName.substring(0, userInputName.length - 1), transactionManager);
+        }
+
+    });
 
 
 
